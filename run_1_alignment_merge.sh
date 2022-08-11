ddc982709e0da45b53ee0ec6298a2024938c105d
ml3958 <ml4640@columbia.edu>
2022-08-11 11:43:56 -0400

    added justmental step for 1.2, 1.3, 1.6 - if exists, skip

--- run_1_alignment_merge.sh
+++ run_1_alignment_merge.sh
@@ -39,15 +39,16 @@ fi
 echo "  1.3 build reference db"[$(date --rfc-3339=seconds)]
 if [ ! -f ${DIR_ALIGNMENT_MASTER}/input.fasta ] || [ ! -s ${DIR_ALIGNMENT_MASTER}/input.fasta ]
 then
-  for i in $(cut -f1 $METADATA)
-    do
-      if [ -f $DIR_INPUT/${i}.faa ]
-        then
-          cat $DIR_INPUT/${i}.faa  >> ${DIR_ALIGNMENT_MASTER}/input.fasta
-        else
-          echo ${i}.faa does not exist
-      fi
-    done
+  # for i in $(cut -f1 $METADATA)
+  #   do
+  #     if [ -f $DIR_INPUT/${i}.faa ]
+  #       then
+  #         cat $DIR_INPUT/${i}.faa  >> ${DIR_ALIGNMENT_MASTER}/input.fasta
+  #       else
+  #         echo ${i}.faa does not exist
+  #     fi
+  #   done
+  echo coping input to ${DIR_ALIGNMENT_MASTER}/input.fasta[$(date --rfc-3339=seconds)]
 else
   echo ${DIR_ALIGNMENT_MASTER}/input.fasta exists, skip coping....[$(date --rfc-3339=seconds)]
 fi
