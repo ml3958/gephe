de86344d426ed063b72b0f37ab135114631baf62
ml3958 <ml4640@columbia.edu>
2023-02-28 10:35:39 -0500

    change_db databse name changed

--- utils/build_db.sh
+++ utils/build_db.sh
@@ -9,26 +9,26 @@ DIR_OUT=${4} # a directory to store intermediate files
 
 # -----------------------------
 echo "  1.1 build reference db"[$(date --rfc-3339=seconds)]
-if [ ! -f ${DIR_OUT}/${PREFIX}_input.fasta ] || [ ! -s ${DIR_OUT}/${PREFIX}_input.fasta ]
+if [ ! -f ${DIR_OUT}/${PREFIX}.faa ] || [ ! -s ${DIR_OUT}/${PREFIX}.faa ]
 then
   for i in $(cut -f1 $METADATA)
     do
       if [ -f $DIR_FAA/${i}.faa ]
         then
-          cat $DIR_FAA/${i}.faa  >> ${DIR_OUT}/${PREFIX}_input.fasta
+          cat $DIR_FAA/${i}.faa  >> ${DIR_OUT}/${PREFIX}.faa
         else
           echo ${i}.faa does not exist
       fi
     done
-  echo coping input to ${DIR_OUT}/${PREFIX}_input.fasta[$(date --rfc-3339=seconds)]
+  echo coping input to ${DIR_OUT}/${PREFIX}.faa[$(date --rfc-3339=seconds)]
 else
-  echo ${DIR_OUT}/${PREFIX}_input.fasta exists, skip coping....[$(date --rfc-3339=seconds)]
+  echo ${DIR_OUT}/${PREFIX}.faa exists, skip coping....[$(date --rfc-3339=seconds)]
 fi
-# rm -rf ${DIR_OUT}/${PREFIX}_input.fasta
+# rm -rf ${DIR_OUT}/${PREFIX}.faa
 # rm -rf ${DIR_OUT}/input_ipr.txt
-if [ ! -f ${DIR_OUT}/input_database.dmnd ] || [ ! -s ${DIR_OUT}/input_database.dmnd ]
+if [ ! -f ${PREFIX}.dmnd ] || [ ! -s ${PREFIX}.dmnd ]
 then
-  diamond makedb --in ${DIR_OUT}/${PREFIX}_input.fasta --db ${DIR_OUT}/${PREFIX}_input_database
+  diamond makedb --in ${DIR_OUT}/${PREFIX}.faa --db ${DIR_OUT}/${PREFIX}
 else
-  echo ${DIR_OUT}/${PREFIX}_input_database.dmnd exists, skip coping....[$(date --rfc-3339=seconds)]
+  echo ${DIR_OUT}/${PREFIX}.dmnd exists, skip coping....[$(date --rfc-3339=seconds)]
 fi
