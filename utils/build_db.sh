7858e717cfc5200ec73808188858079ca4d875cb
ml3958 <ml4640@columbia.edu>
2023-04-07 19:28:04 -0400

    build_db.sh version 2, added INPUT_FILE_PATH, DIR_OUT, PREFIX

--- utils/build_db.sh
+++ utils/build_db.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
 
 # DIR_FAA=/mnt/data1/menghanliu/gephe_jgi/0_data/ # JGI data specific
-DIR_FAA=${1}     # a directory of proteome files
-METADATA=${2}    # a metadata with first column being the target genomes/proteomes of interests
+INPUT_FILE_PATH=${1}     # a directory of proteome files
+DIR_OUT=${2}
 PREFIX=${3}
-DIR_OUT=${4} # a directory to store intermediate files
+# the INPUT_FILE_PATH needs to contain a column names input_path
 
 
 # -----------------------------
 echo "  1.1 build reference db"[$(date --rfc-3339=seconds)]
-if [ ! -f ${DIR_OUT}/${PREFIX}.faa ] || [ ! -s ${DIR_OUT}/${PREFIX}.faa ]
-then
-  for i in $(cut -f1 $METADATA)
-    do
-      if [ -f $DIR_FAA/${i}.faa ]
-        then
-          cat $DIR_FAA/${i}.faa  >> ${DIR_OUT}/${PREFIX}.faa
-        else
-          echo ${i}.faa does not exist
-      fi
-    done
-  echo coping input to ${DIR_OUT}/${PREFIX}.faa[$(date --rfc-3339=seconds)]
-else
-  echo ${DIR_OUT}/${PREFIX}.faa exists, skip coping....[$(date --rfc-3339=seconds)]
-fi
-# rm -rf ${DIR_OUT}/${PREFIX}.faa
-# rm -rf ${DIR_OUT}/input_ipr.txt
-if [ ! -f ${PREFIX}.dmnd ] || [ ! -s ${PREFIX}.dmnd ]
-then
-  diamond makedb --in ${DIR_OUT}/${PREFIX}.faa --db ${DIR_OUT}/${PREFIX}
-else
-  echo ${DIR_OUT}/${PREFIX}.dmnd exists, skip coping....[$(date --rfc-3339=seconds)]
-fi
+# parallel "echo {}" ::: $(awk 'NR==1{for(i=1;i<=NF;i++) if($i=="input_path") col=i} {print $col}' input_2881_5species_per_GTDB_2428gem.txt))
+
+for path in $(awk 'NR==1{for(i=1;i<=NF;i++) if($i=="input_path") col=i} {print $col}' ${INPUT_FILE_PATH})
+do
+  if [ -f myfile.txt ]; then
+    echo coping $path
+    cat $path >> ${DIR_OUT}/${PREFIX}.faa
+  else
+    echo $path not exists
+  fi
+done
+
+
+
+# # -----------------------------
+# echo "  1.1 build reference db"[$(date --rfc-3339=seconds)]
+# if [ ! -f ${DIR_OUT}/${PREFIX}.faa ] || [ ! -s ${DIR_OUT}/${PREFIX}.faa ]
+# then
+#   for i in $(cut -f1 $METADATA)
+#     do
+#       if [ -f $DIR_FAA/${i}.faa ]
+#         then
+#           cat $DIR_FAA/${i}.faa  >> ${DIR_OUT}/${PREFIX}.faa
+#         else
+#           echo ${i}.faa does not exist
+#       fi
+#     done
+#   echo coping input to ${DIR_OUT}/${PREFIX}.faa[$(date --rfc-3339=seconds)]
+# else
+#   echo ${DIR_OUT}/${PREFIX}.faa exists, skip coping....[$(date --rfc-3339=seconds)]
+# fi
+# # rm -rf ${DIR_OUT}/${PREFIX}.faa
+# # rm -rf ${DIR_OUT}/input_ipr.txt
+# if [ ! -f ${PREFIX}.dmnd ] || [ ! -s ${PREFIX}.dmnd ]
+# then
+#   diamond makedb --in ${DIR_OUT}/${PREFIX}.faa --db ${DIR_OUT}/${PREFIX}
+# else
+#   echo ${DIR_OUT}/${PREFIX}.dmnd exists, skip coping....[$(date --rfc-3339=seconds)]
+# fi
