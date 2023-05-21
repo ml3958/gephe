a153e272589ac509b59049073e3a18e435a24db8
ml3958 <ml4640@columbia.edu>
2023-05-21 11:58:15 -0400

    update .abc generation methods

--- run_3a_protein.sh
+++ run_3a_protein.sh
@@ -7,8 +7,6 @@
 # proteins-->POG,
 # POG annotation
 
-
-
 echo "  Select proteins"  [$(date --rfc-3339=seconds)]
 python $gephe_dir/pog/select_proteins.py \
   $DIR_ALIGNMENT $DIR_ASSOCIATION $DIR_POG \
@@ -19,9 +17,32 @@ python $gephe_dir/pog/generate_pp_for_selectedproteins.py \
   $DIR_ASSOCIATION $DIR_POG -p $PREFIX_PROTEIN
 
 
-echo "  .abc file for selected proteins"  [$(date --rfc-3339=seconds)]
-python $gephe_dir/pog/generate_abcfile_for_selectedproteins.py \
-  $DIR_ALIGNMENT $DIR_ASSOCIATION $DIR_POG -p $PREFIX_PROTEIN
+# echo "  .abc file for selected proteins"  [$(date --rfc-3339=seconds)]   ---- Retired in gephe v5
+cut -d, -f1 ${PREFIX_PROTEIN}.csv | seqtk subseq ${DIR_ALIGNMENT_MASTER}/input.fasta > ${PREFIX_PROTEIN}.protseq.faa
+run_diamond_abc(){
+  f=$1
+  if [ ! -f $DIR_POG/${f}.diamond.out ] || [ ! -s $DIR_POG//${f}.diamond.out  ]
+    then
+      echo $f started $(date)
+          diamond blastp \
+          -q ${DIR_POG}/${f}.faa \
+          --out ${DIR_POG}/${f}.diamond.out \
+          --db ${DIR_POG}/${f}.faa \
+          -e ${ALIGNMENT_EVALUE} -k ${ALIGNMENT_MAX} \
+          --query-cover ${ALIGNMENT_QUERY_COVERAGE} \
+          --subject-cover ${ALIGNMENT_SUBJECT_COVERAGE} \
+          --outfmt 6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore \
+	        -b8 -c1
+          echo $f finished $(date)
+    else
+      echo $f already exists
+  fi
+}
+export -f run_diamond
+run_diamond_abc ${PREFIX_PROTEIN}.protseq.faa
+
+cat ${PREFIX_PROTEIN}.protseq.faa.diamond.out| cut -f1,2,13 > ${PREFIX_PROTEIN}.abc
+
 
 
 echo "  mxcload"  [$(date --rfc-3339=seconds)]
