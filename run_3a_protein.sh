ac3faf7e27e9500166106235ff97e8a2ff806865
ml3958 <ml4640@columbia.edu>
2023-05-21 17:11:19 -0400

    update .abc generation methods

--- run_3a_protein.sh
+++ run_3a_protein.sh
@@ -17,33 +17,32 @@ python $gephe_dir/pog/generate_pp_for_selectedproteins.py \
   $DIR_ASSOCIATION $DIR_POG -p $PREFIX_PROTEIN
 
 
-# echo "  .abc file for selected proteins"  [$(date --rfc-3339=seconds)]   ---- Retired in gephe v5
-ll $DIR_POG/${PREFIX_PROTEIN}.csv
-cut -d, -f1 $DIR_POG/${PREFIX_PROTEIN}.csv > $DIR_POG/${PREFIX_PROTEIN}.txt
-seqtk subseq ${DIR_ALIGNMENT_MASTER}/input.fasta $DIR_POG/${PREFIX_PROTEIN}.txt > $DIR_POG/${PREFIX_PROTEIN}.protseq.faa
+echo "  .abc file for selected proteins"  [$(date --rfc-3339=seconds)]   ---- Retired in gephe v5
+if [ ! -f $DIR_POG/${PREFIX_PROTEIN}.txt] && cut -d, -f1 $DIR_POG/${PREFIX_PROTEIN}.csv > $DIR_POG/${PREFIX_PROTEIN}.txt
+if [ ! -f $DIR_POG/${PREFIX_PROTEIN}.protseq.faa ] && seqtk subseq ${DIR_ALIGNMENT_MASTER}/input.fasta $DIR_POG/${PREFIX_PROTEIN}.txt > $DIR_POG/${PREFIX_PROTEIN}.protseq.faa
 run_diamond_abc(){
   f=$1
   if [ ! -f $DIR_POG/${f}.diamond.out ] || [ ! -s $DIR_POG//${f}.diamond.out  ]
     then
       echo $f started $(date)
           diamond blastp \
-          -q ${DIR_POG}/${f}.faa \
-          --out ${DIR_POG}/${f}.diamond.out \
-          --db ${DIR_POG}/${f}.faa \
+          -q ${f} \
+          --out ${f}.diamond.out \
+          --db ${f} \
           -e ${ALIGNMENT_EVALUE} -k ${ALIGNMENT_MAX} \
           --query-cover ${ALIGNMENT_QUERY_COVERAGE} \
           --subject-cover ${ALIGNMENT_SUBJECT_COVERAGE} \
           --outfmt 6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore \
 	        -b8 -c1
-          echo $f finished $(date)
+          echo $f.diamond.out finished $(date)
     else
       echo $f already exists
   fi
 }
 export -f run_diamond_abc
-run_diamond_abc $DIR_POG/${PREFIX_PROTEIN}.protseq.faa
+run_diamond_abc ${DIR_POG}/${PREFIX_PROTEIN}.protseq.faa
 cat $DIR_POG/${PREFIX_PROTEIN}.protseq.faa.diamond.out| cut -f1,2,13 > $DIR_POG/${PREFIX_PROTEIN}.abc
-
+echo "  abc file created"  [$(date --rfc-3339=seconds)]
 
 
 echo "  mxcload"  [$(date --rfc-3339=seconds)]
